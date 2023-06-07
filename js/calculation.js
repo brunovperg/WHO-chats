307dc1f16e15275211942bc04c11db94606645cd
Bruno Pergentino <brunovpergentino@gmail.com>
2023-06-07 16:21:57 -0300

    update

--- js/calculation.js
+++ js/calculation.js
@@ -5,6 +5,8 @@ const numbers = document.querySelectorAll("input[type='number']")
 const options = document.getElementById("options");
 const charts = document.getElementById("chart")
 const resultados = document.querySelector(".resultado")
+const textoResultado = document.querySelector(".texto-resultado")
+
 
 resultados.hidden = true
 
@@ -28,9 +30,13 @@ options.addEventListener("click", createElement)
 options.addEventListener("touchstart", createElement)
 charts.addEventListener("click", createElement)
 charts.addEventListener("touchstart", createElement)
+charts.addEventListener("change", createElement)
+
 
 
 function createElement() {
+    const checkedRadio = document.querySelector("input[type='radio']:checked")
+    console.log(checkedRadio.attributes.dataname.value)
     const years = (document.getElementById("anos").value)
     const months = (document.getElementById("meses").value)
     const totalMonths = ((Number(years) * 12) + Number(months))
@@ -41,6 +47,9 @@ function createElement() {
     } else {
         resultados.innerHTML = ''
         try {
+            textoResultado.innerHTML = `
+           <strong> ${charts.options[charts.selectedIndex].text}</strong> para <strong>${checkedRadio.attributes.dataname.value}</strong> de ${years} anos e ${months} meses
+            `
             resultados.innerHTML = `
                 <tr>
                 <th class="th-1">-3</th>
